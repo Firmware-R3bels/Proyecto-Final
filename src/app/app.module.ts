7273818d05169f9621647f7bdc982ff1a930a490
Manuel  alejandro correa centeno <mcorreamanuel@cesde.net>
2023-06-14 10:09:47 -0500

    Se hace la creacion de componentes tanto de home, menu footer, y ademas se hace la creacion de la carpeta interface, se hace la importacion de boostrap 5 al app web

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
